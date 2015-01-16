0c17b5ceb967cdd334fe64f07896255f19b470e0
Melissa Marshall <mveritym@gmail.com>
2015-01-16 15:49:00 -0800

    #14 [Melissa] Switching protractor to use firefox in the hopes it will make selenium work on snap...

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,5 +1,8 @@
 exports.config = {
   specs: ['test/e2e/*/*.spec.js'],
   baseUrl: 'http://localhost:9001', //default test port with Yeoman
-  framework: 'jasmine2'
+  framework: 'jasmine2',
+  capabilities: {
+    'browserName': 'firefox'
+  },
 }
